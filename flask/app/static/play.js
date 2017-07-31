07e6321ba8c62a1529935b00549a53f156b87b45
Shivendra Panicker <shivendranitb@gmail.com>
2017-07-31 15:48:35 -0400

    play without refreshing map

--- flask/app/static/play.js
+++ flask/app/static/play.js
@@ -2,12 +2,33 @@ $( function() {
 
     var day = 0;
 
+    var playMap = function(){
+    	console.log("play" + day);
+    	day = day + 1;
+    	var taxiData = [];
+	
+		$.getJSON('/realtime/' + day,
+            function(data) {
+              
+			  for(var i=0;i<data.length;i++)
+			  {
+				    taxiData.push(new google.maps.LatLng(data[i]['weekly_zscore.latitude'],data[i]['weekly_zscore.longitude']));
+			  }	  
+			   var pointArray = new google.maps.MVCArray(taxiData);
+	
+				// what data for the heatmap and how to display it
+				heatmap = new google.maps.visualization.HeatmapLayer({
+					data: pointArray,
+					radius: 12
+				});
+
+				// placing the heatmap on the map
+				heatmap.setMap(map);	
+		  });
+    }
+
 	$( "#play" ).click(function() {
 		console.log("starting to play the map");
-		setInterval(function(){
-			console.log("called" + day);
-			day = day + 1;
-			initialize(day);
-			} , 5000);
+		setInterval(playMap, 5000);
 	});
 } );
